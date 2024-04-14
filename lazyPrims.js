7705552d55f0954bf9be53e743c92dd5f3c3ef38
Albina Yermek <yermekalbina@gmail.com>
2024-04-14 22:26:22 +0500

    start node

--- lazyPrims.js
+++ lazyPrims.js
@@ -4,7 +4,9 @@ let network = null;
 let priorityQueue = [];
 let visited = new Set();
 let container = document.getElementById('mst');
-let data = { nodes: nodes, edges: edges }; // Display all edges initially in black
+let data = { nodes: nodes, edges: edges }; 
+let pqContainer = document.getElementById('pq');
+
 let options = {
   edges: {
     color: { inherit: false }
@@ -28,10 +30,14 @@ function addEdge() {
     }
 
     if (!nodes.get(fromNode)) {
-        nodes.add({id: fromNode, label: 'Node ' + fromNode});
+        nodes.add(
+            {id: fromNode, 
+            label: fromNode});
     }
     if (!nodes.get(toNode)) {
-        nodes.add({id: toNode, label: 'Node ' + toNode});
+        nodes.add(
+            {id: toNode, 
+                label: toNode});
     }
 
     try {
@@ -56,17 +62,31 @@ function startVisualization() {
         alert("Please add some edges before starting the visualization.");
         return;
     }
+    
+    const startNodeID = document.getElementById('start-node').value.trim();
+
+    if(startNodeID === ""){
+        alert("choose or enter the start node please!");
+        return;
+    }
+    
     visited.clear();
     priorityQueue = [];
-    let startNode = nodes.get()[0];
+
+    let startNode = nodes.get()[startNodeID];
+
     if (startNode) {
         visit(startNode.id);
         completeMST();
     }
 }
 
-function visit(nodeId) {
+
+function visit(nodeId) {    
     visited.add(nodeId);
+
+    let copyQueue = [];
+
     edges.get({
         filter: function (edge) {
             return (edge.from === nodeId || edge.to === nodeId) && 
@@ -79,13 +99,29 @@ function visit(nodeId) {
             to: edge.to,
             length: parseFloat(edge.label)
         });
+        copyQueue.push({
+            id: edge.id,
+            from: edge.from,
+            to: edge.to,
+            length: parseFloat(edge.label)
+        });
     });
-    priorityQueue.sort((a, b) => a.length - b.length); // Min-heap simulation by sorting
+    priorityQueue.sort((a, b) => a.length - b.length); 
+    copyQueue.sort((a, b) => a.length - b.length); 
+
+    while (copyQueue.length > 0) {
+        const nextItem = copyQueue.shift();
+        console.log(nextItem.id + " " + nextItem.length);
+        var newDiv = document.createElement('newDiv');
+        var value = document.createTextNode(nextItem.id + " " + nextItem.length);
+        newDiv.appendChild(value);
+        pqContainer.appendChild(newDiv);
+    }
 }
 
 function completeMST() {
     while (priorityQueue.length > 0) {
-        let edge = priorityQueue.shift(); // Get the edge with the minimum weight
+        let edge = priorityQueue.shift(); 
         if (!visited.has(edge.from) || !visited.has(edge.to)) {
             edges.update({id: edge.id, color: {color: 'red', highlight: 'red'}, chosen: true});
             let nextNode = visited.has(edge.from) ? edge.to : edge.from;
